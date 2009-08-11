a56c3ab736ff120e81d757c8d68fbb748fe54be5
Henrik Bengtsson <henrikb@braju.com>
2009-08-11 00:00:00 +0000

    version 0.1.8

--- R/readCacheHeader.R
+++ R/readCacheHeader.R
@@ -57,7 +57,7 @@ setMethodS3("readCacheHeader", "default", function(file, ...) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Argument 'file':
   if (inherits(file, "connection")) {
-    pathname <- NULL;
+    pathname <- sprintf("'%s' [description of the opened connection]", summary(file)$description);
   } else {
     pathname <- as.character(file);
     if (!isFile(pathname))
@@ -76,15 +76,20 @@ setMethodS3("readCacheHeader", "default", function(file, ...) {
   header <- list();
 
   # 1a. Load identifier:
-  header$identifier <- readChar(con=file, nchars=64);
-  pattern <- "^Rcache v([0-9][0-9]*[.][0-9][0-9]*([.][0-9][0-9]*)*).*";
-  if (regexpr(pattern, header$identifier) == -1) {
-    throw("Rcache file format error ('", pathname, 
-          "'). Invalid identifier: ", header$identifier);
+  id <- readChar(con=file, nchars=64);
+  if (length(id) == 0) {
+    throw("Rcache file format error. Read empty header identifier: ", 
+                                                                 pathname);
   }
+  pattern <- "^Rcache v([0-9][0-9]*[.][0-9][0-9]*([.][0-9][0-9]*)*).*";
+  if (regexpr(pattern, id) == -1) {
+    throw("Rcache file format error ('", pathname, 
+                                           "'). Invalid identifier: ", id);
+  }
+  header$identifier <- id;
 
   # 1b. Get version
-  header$version <- gsub(pattern, "\\1", header$identifier);
+  header$version <- gsub(pattern, "\\1", id);
 
   # 1c. Read trailing '\0'.
   dummy <- readBin(con=file, what=integer(), size=1, n=1);
@@ -121,6 +126,13 @@ setMethodS3("readCacheHeader", "default", function(file, ...) {
 
 ############################################################################
 # HISTORY:
+# 2009-08-11
+# o Now readCacheHeader() reports the "pathname" in error/warnings messages,
+#   if argument 'file' refers to a file and the "description" if it refers
+#   to a connection.
+# 2009-07-29
+# o ROBUSTNESS: Added sanity check to readCacheHeader() testing that the
+#   read header identifier is non-empty.
 # 2008-02-14
 # o BUG FIX: The throw() for invalid identifiers was trying to put the
 #   connection object is the out and not the identifier.
