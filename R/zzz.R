b4af012ec0e61645e6978918e516464f4402144f
Henrik Bengtsson <henrikb@braju.com>
2013-10-17 00:00:00 +0000

    version 0.9.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,25 +1,22 @@
-# Allows conflicts. For more information, see library() and
-# conflicts() in [R] base.
-.conflicts.OK <- TRUE
-
-
-## .First.lib <- function(libname, pkgname) {
-.onAttach <- function(libname, pkgname) { 
+.onLoad <- function(libname, pkgname) {
+  ns <- getNamespace(pkgname);
   pkg <- Package(pkgname);
-  assign(pkgname, pkg, pos=getPosition(pkg));
-
-  # This will make sure print(R.cache) gives the proper output, iff called.
-  autoload("print.Object", package="R.oo")
-
-  startupMessage(pkg);
+  assign(pkgname, pkg, envir=ns);
 
   # Setup the cache root path, possibly by prompting the user.
   setupCacheRootPath();
 }
 
+.onAttach <- function(libname, pkgname) {
+  pkg <- get(pkgname, envir=getNamespace(pkgname));
+  startupMessage(pkg);
+}
+
 
 ############################################################################
-# HISTORY: 
+# HISTORY:
+# 2013-10-17
+# o CLEANUP: autoload("print.Object", package="R.oo") is no longer needed.
 # 2005-12-09
 # o Created.
 ############################################################################
